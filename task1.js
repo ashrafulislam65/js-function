b9a28704dfa9718ad8e09e957900d4bbbe817360
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-06 19:42:52 +0600

    added task1 multiply 4 numbers

---
+++ task1.js
@@ -0,0 +1,8 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+function multiply(num1, num2, num3, num4)
+{
+   let result = num1*num2*num3*num4;
+   console.log(result);
+}
+multiply(2,2,2,2)
