c3243dbab00d1df7afdb247ba5410c2c7801bd2d
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-07 14:30:16 +0600

    added task4 count the amount of zero in a string

---
+++ task4.js
@@ -0,0 +1,18 @@
+// Task-4
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+function count_zero(str)
+{
+    let count = 0;
+    for (let i=0;i<str.length;i++)
+    { 
+        if(str[i]==='0')
+        {
+            count++;
+        }
+        
+    }
+    return count ;
+
+}
+let result = count_zero('0110010010')
+console.log(result);
